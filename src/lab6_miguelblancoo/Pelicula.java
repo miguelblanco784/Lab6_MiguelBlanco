c935a9bc1a4fb905d013e37d3767d6ea4afcdf66
miguelblanco784 <miguelrobertoblanco@gmail.com>
2019-05-31 14:27:35 -0600

    Medio Camino

---
+++ src/lab6_miguelblancoo/Pelicula.java
@@ -0,0 +1,108 @@
+package lab6_miguelblancoo;
+
+import java.util.ArrayList;
+
+public class Pelicula {
+
+    private String nombre;
+    private int duracion;
+    private String categoria;
+    private ArrayList<String> actores = new ArrayList();
+    private String directores;
+    private String compania;
+    private String idioma;
+    private boolean doblaje;
+    private boolean subtitulos;
+
+    public Pelicula() {
+    }
+
+    public Pelicula(String nombre, int duracion, String categoria, String directores, String compania, String idioma, boolean doblaje, boolean subtitulos) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+        this.categoria = categoria;
+        this.directores = directores;
+        this.compania = compania;
+        this.idioma = idioma;
+        this.doblaje = doblaje;
+        this.subtitulos = subtitulos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public ArrayList<String> getActores() {
+        return actores;
+    }
+
+    public void setActores(ArrayList<String> actores) {
+        this.actores = actores;
+    }
+
+    public String getDirectores() {
+        return directores;
+    }
+
+    public void setDirectores(String directores) {
+        this.directores = directores;
+    }
+
+    public String getCompania() {
+        return compania;
+    }
+
+    public void setCompania(String compania) {
+        this.compania = compania;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public boolean isDoblaje() {
+        return doblaje;
+    }
+
+    public void setDoblaje(boolean doblaje) {
+        this.doblaje = doblaje;
+    }
+
+    public boolean isSubtitulos() {
+        return subtitulos;
+    }
+
+    public void setSubtitulos(boolean subtitulos) {
+        this.subtitulos = subtitulos;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula{" + "nombre=" + nombre + ", duracion=" + duracion + ", categoria=" + categoria + ", actores=" + actores + ", directores=" + directores + ", compania=" + compania + ", idioma=" + idioma + ", doblaje=" + doblaje + ", subtitulos=" + subtitulos + '}';
+    }
+    
+}
---
+++ src/lab6_miguelblancoo/Serie.java
@@ -0,0 +1,108 @@
+package lab6_miguelblancoo;
+
+import java.util.ArrayList;
+
+public class Serie {
+
+    private String nombre;
+    private int duracion;
+    private String categorias;
+    private ArrayList<String> actores = new ArrayList();
+    private int temporadas;
+    private String productora;
+    private String idiomaor;
+    private boolean doblaje;
+    private boolean subtitulos;
+
+    public Serie() {
+    }
+
+    public Serie(String nombre, int duracion, String categorias, int temporadas, String productora, String idiomaor, boolean doblaje, boolean subtitulos) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+        this.categorias = categorias;
+        this.temporadas = temporadas;
+        this.productora = productora;
+        this.idiomaor = idiomaor;
+        this.doblaje = doblaje;
+        this.subtitulos = subtitulos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(String categorias) {
+        this.categorias = categorias;
+    }
+
+    public ArrayList<String> getActores() {
+        return actores;
+    }
+
+    public void setActores(ArrayList<String> actores) {
+        this.actores = actores;
+    }
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public String getProductora() {
+        return productora;
+    }
+
+    public void setProductora(String productora) {
+        this.productora = productora;
+    }
+
+    public String getIdiomaor() {
+        return idiomaor;
+    }
+
+    public void setIdiomaor(String idiomaor) {
+        this.idiomaor = idiomaor;
+    }
+
+    public boolean isDoblaje() {
+        return doblaje;
+    }
+
+    public void setDoblaje(boolean doblaje) {
+        this.doblaje = doblaje;
+    }
+
+    public boolean isSubtitulos() {
+        return subtitulos;
+    }
+
+    public void setSubtitulos(boolean subtitulos) {
+        this.subtitulos = subtitulos;
+    }
+
+    @Override
+    public String toString() {
+        return "Series{" + "nombre=" + nombre + ", duracion=" + duracion + ", categorias=" + categorias + ", actores=" + actores + ", temporadas=" + temporadas + ", productora=" + productora + ", idiomaor=" + idiomaor + ", doblaje=" + doblaje + ", subtitulos=" + subtitulos + '}';
+    }
+
+}
